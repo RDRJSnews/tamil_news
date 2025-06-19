137ce5f3e2ab76b3f432204fefb8e1efb298769c
RDRJSnews <news.rdrjs@gmail.com>
2025-06-19 15:50:19 +0530

    Update news_video.py

--- news_video.py
+++ news_video.py
@@ -7,7 +7,6 @@ import io
 import tempfile
 import os
 from datetime import datetime
-from moviepy.utils import ProgressBar
 
 def log_print(level, message):
     print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [{level}] {message}")
@@ -137,7 +136,7 @@ def repeat_video_to_match_audio(video_path, audio_buffer):
                 codec='libx264',
                 audio_codec='aac',
                 fps=video.fps,
-                logger=ProgressBar()
+                verbose=False
             )
 
             # Read the file into a buffer
